8b98cf10d3eaad844d059c206498b6006091936b
OKAZAKI Takurou <takurou.okazaki@gmail.com>
2012-12-22 14:12:46 +0900

    Fixed tag cloud bug for postgresql

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,8 +8,8 @@ class Item < ActiveRecord::Base
   end
 
   def self.tag_counts
-    Tag.select("tags.*, count(taggings.tag_id) as count").
-      joins(:taggings).group("taggings.tag_id")
+    Tag.select("tags.id, tags.name, count(taggings.tag_id) as count").
+      joins(:taggings).group("taggings.tag_id, tags.id, tags.name")
   end
 
   def tag_list
