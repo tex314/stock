fec41423d8102c426259fe5feee4cb40e75eb808
OKAZAKI Takurou <takurou.okazaki@gmail.com>
2013-02-07 00:15:05 +0900

    Fixed logger

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+require 'active_support/core_ext/numeric/bytes'
 
 # Initialize the rails application
 Stock::Application.initialize!
