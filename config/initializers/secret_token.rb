1dd7b0be8b02f1dd6ca8d9f30d948d0b14ded272
OKAZAKI Takurou <takurou.okazaki@gmail.com>
2013-02-09 18:10:06 +0900

    Use secret token from ENV or generated on boot up

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Stock::Application.config.secret_token = '1931215eba3a2bdc2cd0ca5611c1703290a67adf4bb76a52113c70d2bf0d5440fdcb40d4e37dc65b15142c2706c1dc4e448b0941d2e0950d960aeb3690479898'
+Stock::Application.config.secret_token = ENV['SECRET_TOKEN'] || SecureRandom.hex(64)
